b90994b1385850bd84c30092ccefc2c805c62409
ELÍAS GUILLERMO OCARES GÁLVEZ <e.ocares@alumnos.santotomas.cl>
2021-04-21 20:11:51 -0400

    Agregar archivos de proyecto.

---
+++ ColegioInterfaz/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace ColegioInterfaz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboCursos = new System.Windows.Forms.ComboBox();
+            this.gridAlumnos = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gridProfesor = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gridAlumnos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridProfesor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboCursos
+            // 
+            this.comboCursos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboCursos.FormattingEnabled = true;
+            this.comboCursos.Location = new System.Drawing.Point(69, 152);
+            this.comboCursos.Name = "comboCursos";
+            this.comboCursos.Size = new System.Drawing.Size(121, 21);
+            this.comboCursos.TabIndex = 0;
+            this.comboCursos.SelectedIndexChanged += new System.EventHandler(this.comboCursos_SelectedIndexChanged);
+            // 
+            // gridAlumnos
+            // 
+            this.gridAlumnos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridAlumnos.Location = new System.Drawing.Point(403, 59);
+            this.gridAlumnos.Name = "gridAlumnos";
+            this.gridAlumnos.Size = new System.Drawing.Size(361, 114);
+            this.gridAlumnos.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(552, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Alumnos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(552, 266);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Profesor";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // gridProfesor
+            // 
+            this.gridProfesor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridProfesor.Location = new System.Drawing.Point(403, 296);
+            this.gridProfesor.Name = "gridProfesor";
+            this.gridProfesor.Size = new System.Drawing.Size(154, 51);
+            this.gridProfesor.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gridProfesor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gridAlumnos);
+            this.Controls.Add(this.comboCursos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridAlumnos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridProfesor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboCursos;
+        private System.Windows.Forms.DataGridView gridAlumnos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView gridProfesor;
+    }
+}
+
---
+++ ColegioInterfaz/Form1.cs
@@ -0,0 +1,75 @@
+﻿using ColegioInterfaz.Clases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ColegioInterfaz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ContextBD bd = new ContextBD();
+
+
+            comboCursos.DataSource = bd.Cursos.ToList();
+            comboCursos.DisplayMember = "Nombre";
+
+
+
+            
+
+            //textBoxProfesor.Text = bd.Profesores.Where(profesore => profesore.idProfesor == 1).ToString();
+
+            
+            
+
+
+
+        }
+
+        private void comboCursos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ContextBD bd = new ContextBD();
+
+
+            Curso cursoSeleccionado = (Curso)comboCursos.SelectedItem;
+
+            List<Alumno> alumnosDelCurso = bd.Alumnos.Where(Alumno => Alumno.idCurso == cursoSeleccionado.idCurso).ToList();
+
+            gridAlumnos.DataSource = alumnosDelCurso;
+
+            gridAlumnos.Columns[0].Visible = false;
+            gridAlumnos.Columns[3].Visible = false;
+            gridAlumnos.Columns[4].Visible = false;
+
+            List<Profesore> profesorDelCurso = bd.Profesores.Where(Profesore => Profesore.idProfesor == cursoSeleccionado.idProfesor).ToList();
+
+            gridProfesor.DataSource = profesorDelCurso;
+
+            gridProfesor.Columns[0].Visible = false;
+            gridProfesor.Columns[2].Visible = false;
+
+
+
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
